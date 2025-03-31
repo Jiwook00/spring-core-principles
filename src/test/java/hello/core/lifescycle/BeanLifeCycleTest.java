e1fb0faa087d8d0c638e8fa73d741d3b61a3c1b4
Jiwook00 <jwkim775@gmail.com>
2025-03-31 20:18:25 +0900

    애노테이션 방식

--- src/test/java/hello/core/lifescycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifescycle/BeanLifeCycleTest.java
@@ -17,8 +17,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
-        // destroyMethod = "close"를 사용하지 않으면 추론 기능으로 종료 메서드를 추론해서 호출
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.steUrl("http://hello-spring.dev");
