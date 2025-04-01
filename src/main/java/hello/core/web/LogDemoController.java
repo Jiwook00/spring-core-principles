51a4d71ba3f995712ee8ef0db0ecb099e6d131e9
Jiwook00 <jwkim775@gmail.com>
2025-04-01 18:44:26 +0900

    스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+    // MyLogger를 주입 받는 것이 아니라 MyLogger를 찾을 수 있는 DL이 삽입
 
     @RequestMapping("log-demo:")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURI = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestUrl(requestURI);
 
         myLogger.log("controller test");
